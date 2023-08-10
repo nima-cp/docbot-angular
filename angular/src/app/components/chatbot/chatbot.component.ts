89d305398cf70a1843577595ceadfc7e26a67ba1
nima-cp <nima.k@centropaghe.it>
2023-08-10 09:49:01 +0200

    added id and decimal = 4

--- angular/src/app/components/chatbot/chatbot.component.ts
+++ angular/src/app/components/chatbot/chatbot.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatApiService } from 'src/app/services/chat-api.service';
+import { v4 as uuidv4 } from 'uuid';
+
+interface Message {
+  id?: string;
+  message?: string;
+}
 
 interface Messages {
-  bot: string[];
-  user: string[];
+  bot: Message[];
+  user: Message[];
 }
 
 interface Prompt {
   completion_tokens?: number;
   prompt_tokens?: number;
-  total_cost?: number;
   total_tokens?: number;
+  total_cost?: number;
 }
 
 @Component({
@@ -19,32 +25,47 @@ interface Prompt {
   styleUrls: ['./chatbot.component.css'],
 })
 export class ChatbotComponent implements OnInit {
-  messages: Messages = { bot: [], user: [] };
+  messages: Messages = {
+    bot: [],
+    user: [],
+  };
   new_message: string = '';
   chat_history: [] = [];
-  answer: string = '';
-  prompt: Prompt = {};
+  answer?: string;
+  prompt: Prompt = {
+    completion_tokens: 0,
+    prompt_tokens: 0,
+    total_tokens: 0,
+    total_cost: 0,
+  };
 
   constructor(private ChatApiService: ChatApiService) {}
 
   ngOnInit() {
-    this.messages.bot.push('Benvenuto in DocBot!');
-    this.messages.bot.push('Come posso aiutarti?');
+    this.messages.bot.push({ id: uuidv4(), message: 'Benvenuto in DocBot!' });
+    this.messages.bot.push({ id: uuidv4(), message: 'Come posso aiutarti?' });
+    console.log('conversation', this.messages);
   }
 
   send_message() {
     if (this.new_message.trim() === '') return;
 
-    this.messages.user.push(this.new_message);
+    this.messages.user.push({ id: uuidv4(), message: this.new_message });
 
     this.ChatApiService.getChatResponse(this.new_message)
       .then((response) => {
         this.chat_history = response.data.chat_history;
         this.answer = response.data.response.result.answer;
-        this.messages.bot.push(this.answer);
+        this.messages.bot.push({
+          id: uuidv4(),
+          message: this.answer,
+        });
 
         this.prompt = response.data.response.prompt;
 
+        console.log('conversation', this.messages);
+        console.log('chat_history', this.chat_history);
+        console.log('prompt', this.prompt);
         console.log(response);
       })
       .catch((error) => {
@@ -52,9 +73,5 @@ export class ChatbotComponent implements OnInit {
       });
 
     this.new_message = '';
-
-    console.log('conversation', this.messages);
-    console.log('chat_history', this.chat_history);
-    console.log('prompt', this.prompt);
   }
 }
